a52914f762e11ed5dc0dd61efdb0b0bafe277f35
Thejaswini M S <tejaswinimvsr@gmail.com>
2023-05-31 15:43:40 +0530

    Time: 57 ms (57.00%) | Memory: 42.3 MB (16.78%) - LeetSync

--- Leetcode/array-prototype-last/array-prototype-last.js
+++ Leetcode/array-prototype-last/array-prototype-last.js
@@ -1,5 +1,6 @@
 Array.prototype.last = function() {
-    return this.length=== 0 ?-1 : this[this.length - 1];
+    const arrLength = this.length;
+    return arrLength=== 0 ?-1 : this[arrLength - 1];
 };
 
 const arr = [1, 2, 3];
