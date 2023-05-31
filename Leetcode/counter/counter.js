d79b7dc1280a275b2aec15eb69fb982f7b6bc4ca
Thejaswini M S <tejaswinimvsr@gmail.com>
2023-05-31 16:09:47 +0530

    Time: 64 ms (17.74%) | Memory: 41.5 MB (90.35%) - LeetSync

--- Leetcode/counter/counter.js
+++ Leetcode/counter/counter.js
@@ -3,10 +3,7 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
-    var count = n;
-    return function() {
-        return count++;
-    };
+    return () => { return n++}
 };
 
 /** 
